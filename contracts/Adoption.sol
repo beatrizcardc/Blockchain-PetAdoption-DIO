6b0decc4fbdc681448c1296e09880f58229ae531
beatrizcardc <beacarcun@gmail.com>
2022-07-26 10:12:52 -0300

    função adopt e getAdopters

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.5.0;  //o erro não tem problema
 contract Adoption{
 
   address[16] public adopters;
-
+//função para registrar a adoção
   function adopt(uint petId) public returns (uint){
 
     adopters[petId] = msg.sender; // msg é quem escreve na blockchain - dono da transição - remetente
@@ -12,7 +12,7 @@ contract Adoption{
     return petId;
 //essa função permitirá a adoção dos pets
   }
-
+//função para obter a lista de usuarios adotantes - que em lugar do nome é o endereço da criptomoeda
   function getAdopters() public returns (address[16] memory) {
 //memory guarda os dados no contrato
     return adopters;
